8d79bd7c84c789af9577938f99b083d5508713b6
Lauren Davis <londond9218@gmail.com>
2015-09-10 08:36:43 -0600

    first commit

---
+++ new.js
@@ -0,0 +1,14 @@
+angular.module('app', []);
+
+var mainController = function ( $scope, $timeout ) {
+	$scope.editText = function ( event ) {
+		$scope.edit = !$scope.edit;
+		if ( $scope.edit ) {
+
+		}
+
+	}
+
+}
+
+angular.modul('app', []).controller('mainController', ['$scope', 'mainController'];
